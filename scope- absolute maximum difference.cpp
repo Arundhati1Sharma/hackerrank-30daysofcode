f86cd0aa9c3a18a91afbfc15c2312541be120999
Arundhati  Sharma <arundhatisharma1098@gmail.com>
2020-07-17 10:23:08 +0530

    Create scope- absolute maximum difference.cpp

---
+++ scope- absolute maximum difference.cpp
@@ -0,0 +1,54 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Difference {
+    private:
+    vector<int> elements;
+  
+  	public:
+  	int maximumDifference;
+
+	Difference(vector<int> ele){
+        elements=ele;
+    }
+    void computeDifference(){
+        int n=elements.size();
+int min=101;
+int max=0;
+
+for(int i=0;i<n;++i)
+{
+    if(elements[i]<min){min=elements[i];}
+    if(elements[i]>max){max=elements[i];}
+}
+maximumDifference=max-min;
+}
+
+}; // End of Difference class
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> a;
+    
+    for (int i = 0; i < N; i++) {
+        int e;
+        cin >> e;
+        
+        a.push_back(e);
+    }
+    
+    Difference d(a);
+    
+    d.computeDifference();
+    
+    cout << d.maximumDifference;
+    
+    return 0;
+}
